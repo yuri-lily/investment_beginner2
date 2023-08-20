76d8d1cd289da0f42812a64e165eb58383bbf2c6
yuri-lily <yuri.lilyy.xx@gmail.com>
2023-08-20 09:33:42 +0900

    sign up, log in

---
+++ app/models/gender.rb
@@ -0,0 +1,12 @@
+class Gender < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '男性' },
+    { id: 3, name: '女性' },
+    { id: 4, name: '非公開' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  extend  ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :age
+  belongs_to :gender
+
+  validates :name, presence: true
+  validates :age_id, :gender_id, numericality: { other_than: 1, message: "can't be blank" }
+
+end
